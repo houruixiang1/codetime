3e33ccac7612efec8f8ca078c8a9dbe368ef40cc
houruixiang1 <1302897902@qq.com>
2021-10-28 09:50:14 +0800

    修改15题

--- JsSpider/yuanrenxue/15.py
+++ JsSpider/yuanrenxue/15.py
@@ -7,7 +7,8 @@ import random
 t1 = int(int(time.time()*1000)/1000/2)
 t2 = t1 - int(random.random()*50) +1
 # print(t1,t2)
-vm = pywasm.load(r"D:\pycharmproject\main.wasm")
+# 修改路径
+vm = pywasm.load(r"C:\CodeTime\js\main.wasm")
 result = vm.exec("encode",[t1, t2])
 m = str(result) + '|' + str(t1) + '|' + str(t2)
 list = []
@@ -15,7 +16,8 @@ for i in range(1,6):
     url = 'https://match.yuanrenxue.com/api/match/15?m={}&page={}'.format(m,i)
     headers = {
         'user-agent': 'yuanrenxue.project',
-        'cookie': 'sessionid=zgm0rlsvh3lgtakbwshm3tolg7ompi53;',
+        # 每个电脑都有自己的seesionid
+        'cookie': 'sessionid=6hbehfd0w4y3f0g3h43lyzp6qcedyuyj;',
         'Referer': 'https://match.yuanrenxue.com/match/15'
     }
     resp = requests.get(url = url,headers=headers).json()
