b7ff8b1c2e37a06971c94b09efc56978da4fb096
houruixiang1 <1302897902@qq.com>
2021-11-05 18:31:14 +0800

    add yuanrenxue one

--- JsSpider/yuanrenxue/1.py
+++ JsSpider/yuanrenxue/1.py
@@ -1,3 +1,14 @@
 import requests
 import execjs
+import time
 
+
+timestamp = int(time.time())*1000
+print(timestamp)
+node= execjs.get()
+ctc = node.compile(open(r'C:\CodeTime\JsSpider\yuanrenxue\1.py',mode='r',encoding='utf-8').read())
+funcName = 'get_m("{0}")'.format(timestamp)
+m = ctc.eval(funcName)
+# print(m)
+m = m + '|' + timestamp/1000
+print(m)
