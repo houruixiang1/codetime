5e85f6ed80bf7af2ff5dea5e7970e3be56a01d98
houruixiang1 <1302897902@qq.com>
2021-11-22 11:48:41 +0800

    完美解决刺猬猫小说

--- JsSpider/ciweimao/one_xiaoshuo.py
+++ JsSpider/ciweimao/one_xiaoshuo.py
@@ -10,12 +10,12 @@ def get_vid(url):
     resp = requests.get(url=url,headers=headers).text
     html = etree.HTML(resp)
     # print(html)
-    name = html.xpath('//ul[@class="book-chapter-list"]/li[2]/a/text()')
-    link = html.xpath('//ul[@class="book-chapter-list"]/li[2]/a/@href')
+    name = html.xpath('//ul[@class="book-chapter-list"]/li[1]/a/text()')
+    link = html.xpath('//ul[@class="book-chapter-list"]/li[1]/a/@href')
     name = name[0].strip()
     vid = link[0].split('/')[-1]
-    # print(vid)
-    return name,link,vid
+    # print(name)
+    return (name,link,vid)
 
 def get_content_and_key(vid):
     session = requests.session()
@@ -29,24 +29,16 @@ def get_content_and_key(vid):
     # session.headers = header
     resp1 = session.post(url =url1,data=data1,headers=header)
     response = resp1.json()
-    # cook = str(resp1.cookies)
-    # ci_session = re.search(' (.*?) ',cook,re.S).group(1).strip()
-    # print(ci_session)
-    # key = ci_session.split('=')[0]
-    # value = ci_session.split('=')[1]
-    # print(key,value)
-    # session.cookies.set(key,value)
-    # print(session.cookies)
     chapter_access_key = response['chapter_access_key']
     # print(chapter_access_key)
     url = 'https://www.ciweimao.com/chapter/get_book_chapter_detail_info'
     headers = {
         'Host': 'www.ciweimao.com',
-        'Origin': 'https://www.ciweimao.com',
-        'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+        # 'Origin': 'https://www.ciweimao.com',
+        # 'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.54 Safari/537.36',
         'Referer': 'https://www.ciweimao.com/chapter/{}'.format(vid),
-        'X-Requested-With': 'XMLHttpRequest'
+        # 'X-Requested-With': 'XMLHttpRequest'
     }
     data = {
         'chapter_id': vid,
@@ -56,28 +48,29 @@ def get_content_and_key(vid):
     # print(resp)
     chapter_content = resp['chapter_content']
     encryt_keys = resp['encryt_keys']
-    # print(encryt_keys)
-    return chapter_access_key,chapter_content,encryt_keys
+    return (chapter_access_key,chapter_content,encryt_keys)
 
 def main(accesskey,content,key):
     with open(r'C:\CodeTime\js\ceweimao.js',mode='r',encoding='utf-8')as f:
         js = f.read()
     ctc = execjs.compile(js,cwd=r'C:\clip-spider-v2\node_modules')
-    funcName = 'get_data()'
-    # funcName = 'get_data("{0}","{1}","{2}")'.format(accesskey,content,key)
+    # funcName = 'get_data()'
+    funcName = 'get_data("{0}","{1}","{2}")'.format(accesskey,content,key)
     data = ctc.eval(funcName)
     return data
 
 if __name__ == '__main__':
-    start_url = 'https://www.ciweimao.com/book/100276252'
-    name = get_vid(start_url)[0]
-    # link = get_vid(start_url)[1]
-    vid = get_vid(start_url)[2]
-    # print(name,vid)
-    get_content_and_key(vid)
-    accesskey = get_content_and_key(vid)[0]
-    content = get_content_and_key(vid)[1]
-    key = get_content_and_key(vid)[2]
-    print(accesskey,content,key)
+    start_url = 'https://www.ciweimao.com/book/100282110'
+    rep = get_vid(start_url)
+    name = rep[0]
+    link = rep[1]
+    vid = rep[2]
+    # print(name,link,vid)
+    value = get_content_and_key(vid)
+    accesskey = value[0]
+    content = value[1]
+    key = value[2]
+    # print(accesskey,content,key)
     real_data = main(accesskey,content,key)
+    real_data = str(real_data).replace("<p class='chapter'>　　","").replace("<span>3lQmQS</span></p>",'').strip()
     print(real_data)
